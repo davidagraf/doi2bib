931022b306c5b77adb1a531a10b3b9bc6c8fe54b
David Graf <davidagraf@gmail.com>
2014-05-19 17:30:35 +0200

    simple call to doi.org

--- test.js
+++ test.js
@@ -1,32 +1,17 @@
 'use strict';
 
 var 
-querystring = require('querystring'),
-Q = require('q'),
 request = require('request'),
-xml2js = require('xml2js'),
-path = require('JSONPath'),
 
-crossRefKey = 'fx.coudert@chimie-paristech.fr', // TODO ?
 doi = '10.1158/0008-5472.CAN-09-1089',
 
-params = {
-  'id' : 'doi:' + doi,
-  'noredirect' : 'true',
-  'pid' : crossRefKey,
-  'format' : 'unixref'
+options = {
+  url: 'http://dx.doi.org/' + doi,
+  headers: {
+    'Accept': 'application/x-bibtex; charset=utf-8'
+  }
 };
 
-(function() {
-  var deferred = Q.defer();
-  request('http://www.crossref.org/openurl?' + querystring.stringify(params),
-          function(error, response, body) {
-    xml2js.parseString(body, {trim: true, explicitArray: false}, function(err, result){
-      deferred.resolve(result);
-    });
-  });
-  return deferred.promise;
-})()
-.then(function(crossref) {
-  console.log(JSON.stringify(crossref, null, 2));
+request(options, function(error, response, body) {
+  console.log(body); 
 });
