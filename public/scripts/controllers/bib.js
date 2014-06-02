ee1131b3f1edd12be6eb03beb6e019801898e0ce
David Graf <davidagraf@gmail.com>
2014-06-02 08:10:59 +0200

    spinning icon

--- public/scripts/controllers/bib.js
+++ public/scripts/controllers/bib.js
@@ -4,6 +4,8 @@ angular.module('Doi2BibApp')
 .controller('BibCtrl', ['$scope', '$http', 'Latex',
     function($scope, $http, Latex) {
   $scope.toBib = function() {
+    $scope.workinprogress = true;
+    $scope.error = $scope.bib = undefined;
     $http({ 
       method: 'GET',
       url: '/doi2bib',
@@ -13,11 +15,11 @@ angular.module('Doi2BibApp')
     }).
     success(function(data) {
       $scope.bib = Latex.indentBib(Latex.encode(Latex.removeNA(data.trim())));
-      $scope.error = undefined;
+      $scope.workinprogress = false;
     }).
     error(function(data/*, status, headers, config*/) {
       $scope.error = data;
-      $scope.bib = undefined;
+      $scope.workinprogress = false;
     });
   };
 }]);
