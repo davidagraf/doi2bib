3f942ef79c109233b4381f3ee27096c1b928a195
David Graf <davidagraf@gmail.com>
2014-06-10 09:48:51 +0200

    use new bibclass to parse and print

--- public/scripts/lib/parsertest.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-(function() {
-  //var example = "@article{10.1007/springerreference\_6343, url={http://dx.doi.org/10.1007/springerreference\_6343}, DOI={10.1007/springerreference\_6343}, journal={SpringerReference}, publisher={Springer-Verlag}, year={2011} }";
-  var example = '@article{aidee, a={b}, c={d,e,f}}';
-
-  console.log(bibparser.parse(example));
-})();
